8ecbc717805fcd627fc6233ca2e11557ddc918df
ziyuan <hanziyuan08@outlook.com>
2018-12-07 00:44:38 +0800

    doc: 增加注释

--- src/main/java/com/github/bpazy/id/IdWorker.java
+++ src/main/java/com/github/bpazy/id/IdWorker.java
@@ -11,6 +11,17 @@ public class IdWorker {
     private long workerId;
     private long datacenterId;
     private long sequence;
+    private long twepoch = 1288834974657L;
+    private long workerIdBits = 5L;
+    private long datacenterIdBits = 5L;
+    private long maxWorkerId = ~(-1L << workerIdBits);  // 计算workerIdBots位可以储存的最大正整数，这里及5位可以储存的最大正整数31
+    private long maxDatacenterId = ~(-1L << datacenterIdBits); // 同上，31
+    private long sequenceBits = 12L;
+    private long workerIdShift = sequenceBits;
+    private long datacenterIdShift = sequenceBits + workerIdBits;
+    private long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+    private long sequenceMask = ~(-1L << sequenceBits); // 同maxWorkerId的计算方法，4095
+    private long lastTimestamp = -1L;
 
     public IdWorker(long workerId, long datacenterId, long sequence) {
         // sanity check for workerId
@@ -28,21 +39,6 @@ public class IdWorker {
         this.sequence = sequence;
     }
 
-    private long twepoch = 1288834974657L;
-
-    private long workerIdBits = 5L;
-    private long datacenterIdBits = 5L;
-    private long maxWorkerId = ~(-1L << workerIdBits);
-    private long maxDatacenterId = ~(-1L << datacenterIdBits);
-    private long sequenceBits = 12L;
-
-    private long workerIdShift = sequenceBits;
-    private long datacenterIdShift = sequenceBits + workerIdBits;
-    private long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
-    private long sequenceMask = ~(-1L << sequenceBits);
-
-    private long lastTimestamp = -1L;
-
     public synchronized long nextId() {
         long timestamp = timeGen();
 
@@ -53,7 +49,7 @@ public class IdWorker {
         }
 
         if (lastTimestamp == timestamp) {
-            sequence = (sequence + 1) & sequenceMask;
+            sequence = (sequence + 1) & sequenceMask; // 通过位运算保证sequence的范围始终在[0, sequenceMask(4095)]
             if (sequence == 0) {
                 timestamp = tilNextMillis(lastTimestamp);
             }
